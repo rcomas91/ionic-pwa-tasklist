2ed2860d6386a36f5677281b3b2a821400cc0eae
rcomas <raydelcomas1991@gmail.com>
2021-02-06 19:36:27 +0100

    deleted files

--- src/app/models/lista-item.model.ts
+++
@@ -1,9 +0,0 @@
-export class ListaItem{
-    desc:string;
-    completado:boolean;
-
-    constructor(desc:string){
-        this.desc=desc;
-        this.completado=false;
-    }
-}
--- src/app/pipes/filtro-completado.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FiltroCompletadoPipe } from './filtro-completado.pipe';
-
-describe('FiltroCompletadoPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FiltroCompletadoPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
