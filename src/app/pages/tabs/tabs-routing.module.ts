cd3658cc14efe57fb486bc2212b932e31c6298af
rcomas91 <74781742+rcomas91@users.noreply.github.com>
2021-02-17 08:06:50 -0500

    Update tabs-routing.module.ts

--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -17,6 +17,10 @@ const routes: Routes = [
         path: 'tab2',
         loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
       },
+       {
+        path: 'tab3',
+        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+      },
       {
       path: 'tab1/agregar/:listaId',
         loadChildren: () => import( '../agregar/agregar.module').then(m => m.AgregarPageModule)
