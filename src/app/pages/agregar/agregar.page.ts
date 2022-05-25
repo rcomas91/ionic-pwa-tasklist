688833e8c1d2e5165de2ce2cb0561853985522c7
rcomas91 <raydelcomas1991@gmail.com>
2022-05-25 17:00:29 -0600

    addind option edit task

--- src/app/pages/agregar/agregar.page.ts
+++ src/app/pages/agregar/agregar.page.ts
@@ -1,50 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ListaItem } from 'src/app/models/lista-item.model';
-import { Lista } from 'src/app/models/lista.model';
-import { DeseosService } from 'src/app/services/deseos.service';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { ListaItem } from "src/app/models/lista-item.model";
+import { Lista } from "src/app/models/lista.model";
+import { DeseosService } from "src/app/services/deseos.service";
+import { AlertController, IonList } from "@ionic/angular";
 
 @Component({
-  selector: 'app-agregar',
-  templateUrl: './agregar.page.html',
-  styleUrls: ['./agregar.page.scss'],
+  selector: "app-agregar",
+  templateUrl: "./agregar.page.html",
+  styleUrls: ["./agregar.page.scss"],
 })
 export class AgregarPage implements OnInit {
-
   lista: Lista;
-  nombreItem = '';
+  nombreItem = "";
+  @ViewChild(IonList) lista2: IonList;
 
-  constructor( private deseosService: DeseosService,
-               private route: ActivatedRoute ) {
-
-    const listaId = this.route.snapshot.paramMap.get('listaId');
-    this.lista = this.deseosService.obtenerLista( listaId );
-
+  constructor(
+    private alertCtrl: AlertController,
+    private deseosService: DeseosService,
+    private route: ActivatedRoute
+  ) {
+    const listaId = this.route.snapshot.paramMap.get("listaId");
+    this.lista = this.deseosService.obtenerLista(listaId);
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   agregarItem() {
-
-    if ( this.nombreItem.length === 0 ) {
+    if (this.nombreItem.length === 0) {
       return;
     }
 
-    const nuevoItem = new ListaItem( this.nombreItem );
-    this.lista.items.push( nuevoItem );
+    const nuevoItem = new ListaItem(this.nombreItem);
+    this.lista.items.push(nuevoItem);
 
-    this.nombreItem = '';
+    this.nombreItem = "";
     this.deseosService.guardarStorage();
   }
 
-  cambioCheck( item: ListaItem ) {
+  cambioCheck(item: ListaItem) {
+    const pendientes = this.lista.items.filter(
+      (itemData) => !itemData.completado
+    ).length;
 
-    const pendientes = this.lista.items
-                            .filter( itemData => !itemData.completado )
-                            .length;
-
-    if ( pendientes === 0 ) {
+    if (pendientes === 0) {
       this.lista.terminadaEn = new Date();
       this.lista.terminada = true;
     } else {
@@ -55,16 +54,48 @@ export class AgregarPage implements OnInit {
     this.deseosService.guardarStorage();
 
     console.log(this.deseosService.listas);
-
   }
 
-  borrar( i: number ) {
-
-    this.lista.items.splice( i, 1 );
+  borrar(i: number) {
+    this.lista.items.splice(i, 1);
     this.deseosService.guardarStorage();
-
   }
-  
 
-
+  async editarTarea(i) {
+    const alert = await this.alertCtrl.create({
+      header: "Editar tarea",
+      inputs: [
+        {
+          name: "desc",
+          type: "text",
+          value: i.desc,
+          placeholder: "Tarea a editar",
+        },
+      ],
+      buttons: [
+        {
+          text: "Cancelar",
+          role: "cancel",
+          handler: () => {
+            console.log("Cancelar");
+            this.lista2.closeSlidingItems();
+          },
+        },
+        {
+          text: "Actualizar",
+          handler: (data) => {
+            console.log(data);
+            if (data.desc.length === 0) {
+              this.borrar(i);
+            }
+            i.desc = data.desc;
+            this.deseosService.guardarStorage();
+            this.lista2.closeSlidingItems();
+          },
+        },
+      ],
+    });
+    alert.present();
+    // this.router.navigateByUrl("/tabs/agregar");
+  }
 }
--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -1,55 +1,51 @@
-import { Injectable } from '@angular/core';
-import { ListaItem } from '../models/lista-item.model';
-import { Lista } from '../models/lista.model';
+import { Injectable } from "@angular/core";
+import { ListaItem } from "../models/lista-item.model";
+import { Lista } from "../models/lista.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class DeseosService {
-
-  listas:Lista[]=[];
-
-  constructor() { 
+  listas: Lista[] = [];
+  tarea: ListaItem;
+  constructor() {
     this.cargarStorage();
-    
-
-
 
     console.log(this.listas);
-
   }
 
-  crearLista(titulo:string){
-    const nuevalista=new Lista(titulo);
+  crearLista(titulo: string) {
+    const nuevalista = new Lista(titulo);
     this.listas.push(nuevalista);
     this.guardarStorage();
 
     return nuevalista.id;
   }
 
-  borrarLista( lista:Lista ){
-    this.listas=this.listas.filter(listaData=>listaData.id!==lista.id);
+  borrarLista(lista: Lista) {
+    this.listas = this.listas.filter((listaData) => listaData.id !== lista.id);
     this.guardarStorage();
   }
 
-  obtenerLista(id:string | number){
-    id= Number(id);
-    return this.listas.find(listaData=>
-       listaData.id===id);
+  obtenerLista(id: string | number) {
+    id = Number(id);
+    return this.listas.find((listaData) => listaData.id === id);
   }
+
+  obtenertarea(tarea: string) {
+    return this.tarea.desc;
+  }
+
+  guardarStorage() {
+    localStorage.setItem("data", JSON.stringify(this.listas));
+  }
+
   
-
-  guardarStorage(){
-      localStorage.setItem('data',JSON.stringify(this.listas));
+  cargarStorage() {
+    if (localStorage.getItem("data")) {
+      this.listas = JSON.parse(localStorage.getItem("data"));
+    } else {
+      this.listas = [];
+    }
   }
-  cargarStorage(){
-    if (localStorage.getItem('data')){
-      this.listas=JSON.parse(localStorage.getItem('data'));
-  }else{
-    this.listas=[];
-  }
-}
-
-
-
 }
